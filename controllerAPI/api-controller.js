24d6d22c7f9a246cf99ecc5f4b298772fe524360
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-09-21 17:31:30 +1100

    new index

--- controllerAPI/api-controller.js
+++ controllerAPI/api-controller.js
@@ -1,18 +1,17 @@
-var dbcon = require('../crowdfunding_db');
+// api-controller.js
 
+var dbcon = require('../crowdfunding_db');
 var connection = dbcon.getconnection();
 connection.connect();
 
 var express = require('express');
-
 var router = express.Router();
 
+// Retrieve all fundraisers (initial existing route)
 router.get('/', (req, res) => {
-    connection.query('SELECT * FROM fundraiser', (err, rows, fields) => {
+    connection.query('SELECT * FROM fundraiser', (err, rows) => {
         if (err) {
-            console.error("Error while performing Query:", {
-                error: err
-            });
+            console.error("Error while performing query:", err);
             res.status(500).send("Error while performing query.");
         } else {
             res.send(rows);
@@ -20,5 +19,23 @@ router.get('/', (req, res) => {
     });
 });
 
-module.exports = router;
+// Retrieve all active fundraisers with category
+router.get('/active-fundraisers', (req, res) => {
+    const query = `
+        SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, f.ACTIVE, c.NAME as CATEGORY
+        FROM fundraiser f
+        JOIN category c ON f.CATEGORY_ID = c.CATEGORY_ID
+        WHERE f.ACTIVE = 1;
+    `;
 
+    connection.query(query, (err, rows) => {
+        if (err) {
+            console.error("Error while performing query:", err);
+            res.status(500).send("Error while performing query.");
+        } else {
+            res.json(rows);
+        }
+    });
+});
+
+module.exports = router;
