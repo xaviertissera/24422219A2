4ccadf2d8cc7e2c59325b249fde917c05e59b4af
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-09-22 01:23:32 +1100

    index.html finalized file
    to make the website more attractive i have integrated a free template with my project work as this program is not about designing i thought there is no point of developing css from the scratch.

--- controllerAPI/api-controller.js
+++ controllerAPI/api-controller.js
@@ -67,4 +67,25 @@ router.get('/search-fundraisers', (req, res) => {
     });
 });
 
+
+// Retrieve fundraiser details by ID
+router.get('/fundraiser', (req, res) => {
+    const { id } = req.query;
+    const query = `
+        SELECT f.FUNDRAISER_ID, f.ORGANIZER, f.CAPTION, f.TARGET_FUNDING, f.CURRENT_FUNDING, f.CITY, c.NAME as CATEGORY
+        FROM fundraiser f
+        JOIN category c ON f.CATEGORY_ID = c.CATEGORY_ID
+        WHERE f.FUNDRAISER_ID = ${id}
+    `;
+
+    connection.query(query, (err, rows) => {
+        if (err) {
+            console.error("Error while performing query:", err);
+            res.status(500).send("Error while performing query.");
+        } else {
+            res.json(rows[0]); // Return only one result
+        }
+    });
+});
+
 module.exports = router;
